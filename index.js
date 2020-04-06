832316aab25cf2ea883ac71c4822ca7c764e6edb
Tyler Kummer <tylerkummer@protonmail.com>
2020-04-06 17:16:47 -0500

    If Else Updates

--- index.js
+++ index.js
@@ -19,24 +19,81 @@ server.get("/", (req, res) => {
 server.post("/api/users", (req, res) => {
   const userInfo = req.body;
 
-  users.push(userInfo);
-
-  res.status(201).json(users);
+  if (userInfo.name === null && userInfo.bio === null) {
+    res
+      .status(400)
+      .json({ message: "Please provide name and bio for the user." });
+  } else if (userInfo) {
+    users.push(req.body);
+    res.status(201).json(users);
+  } else if (!userInfo) {
+    res.status(500).json({
+      message: "There was an error while saving the user to the database",
+    });
+  }
 });
 
 server.get("/api/users", (req, res) => {
-  res.json(users);
+  if (!users) {
+    res
+      .status(500)
+      .json({ message: "The users information could not be retrieved." });
+  } else {
+    res.json(users);
+  }
 });
 
 server.get("/api/users/:id", (req, res) => {
+  const userInfo = req.body;
   const id = req.params.id;
-
   const user = users.find((user) => user.id == id);
 
-  if (user) {
-    res.json(user);
+  if (id != userInfo.id) {
+    res
+      .status(404)
+      .json({ message: "The user with the specified ID does not exist." });
+  } else if (!user) {
+    res
+      .status(500)
+      .json({ message: "The user information could not be retrieved." });
   } else {
-    res.status(404).json({ message: "User not found" });
+    res.json(user);
+  }
+});
+
+server.delete("/api/users/:id", (req, res) => {
+  const userInfo = req.body;
+  const id = req.params.id;
+  const user = users.filter((user) => user.id !== id);
+
+  if (id != userInfo.id) {
+    res
+      .status(404)
+      .json({ message: "The user with the specified ID does not exist." });
+  } else if (!user) {
+    res.status(500).json({ message: "The user could not be removed." });
+  } else {
+    res.json(user);
+  }
+});
+
+server.put("/api/users/:id", (req, res) => {
+  const userInfo = req.body;
+  const id = req.params.id;
+  const user = users.filter((user) => user.id !== id);
+
+  if (id != userInfo.id) {
+    res
+      .status(404)
+      .json({ message: "The user with the specified ID does not exist." });
+  } else if (userInfo.name === null && userInfo.bio === null) {
+    res
+      .status(400)
+      .json({ message: "Please provide name and bio for the user." });
+  } else if (!user) {
+    res.status(500).json({ message: "The user could not be removed." });
+  } else if (user) {
+    res.json(user);
   }
 });
 
